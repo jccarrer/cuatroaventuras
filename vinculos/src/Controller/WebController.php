45153d0e1f6f43d9c20a3b950f5db8757ef09f23
jccarrer <carrera1995@hotmail.es>
2018-10-16 13:47:00 -0500

    second commit

---
+++ vinculos/src/Controller/WebController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebController extends Controller
+{
+    /**
+     * @Route("/public/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('web/home.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+
+
+
+    /**
+     * @Route("/productos", name="productos")
+     */
+    public function productos()
+    {
+        return $this->render('web/productos.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/contactenos", name="contactenos")
+     */
+    public function contactenos()
+    {
+        return $this->render('web/contactenos.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+    /**
+     * @Route("/nosotros", name="nosotros")
+     */
+    public function nosotros()
+    {
+        return $this->render('web/nosotros.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+
+
+}
